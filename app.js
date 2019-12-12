5ec481b47da23a552360e371abe9d7b2f9f3a1d6
surjak <surjak1999@gmail.com>
2019-12-12 21:50:28 +0100

    basic structure done

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Works");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+const authRoutes = require("./routes/auth");
+
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect("mongodb://localhost/CoursesProject", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result => {
+    app.listen(8080, () => {
+      console.log("Listening on 8080");
+    });
+  })
+  .catch(err => console.log(err));
